9bbdf086f5024d2e01afab7785a6c7c1ecfa86cf
VGimonnet <valentin.gimonnet18@gmail.com>
2020-03-10 11:06:32 +0100

    Ajout route put api fenetre

--- src/Controller/ApiFenetreController.php
+++ src/Controller/ApiFenetreController.php
@@ -60,8 +60,6 @@ class ApiFenetreController extends AbstractController
         //Ajout d'une nouvelle fenêtre
         $em = $this->getDoctrine()->getManager();
 
-        $fenetreValide = true;
-
         if( $request->get('url') != null && $request->get('width') != null && $request->get('height') != null && $request->get('posx') != null && $request->get('posy') != null )
         {
             $fenetre = new Fenetre();
@@ -74,15 +72,10 @@ class ApiFenetreController extends AbstractController
             $em->persist($fenetre);
             $em->flush();
         } else {
-            $fenetreValide = false;
-        }
-
-        if($fenetreValide){
-            return $this->json(['reponse' => 'Fenêtre enregistrée avec succès']);
-        } else {
             return $this->json(['reponse' => 'Erreur lors de l\'enregistrement de la fenêtre']);
         }
-
+        
+        return $this->json(['reponse' => 'Fenêtre enregistrée avec succès']);        
     }
 
     /**
@@ -90,6 +83,29 @@ class ApiFenetreController extends AbstractController
      */
     public function putFenetre(Request $request){
         //Mise à jour d'une fenêtre
+        $em = $this->getDoctrine()->getManager();
+
+        if( $request->get('id') != null && $request->get('url') != null && $request->get('width') != null && $request->get('height') != null && $request->get('posx') != null && $request->get('posy') != null )
+        {
+            $fenetre = $em->getRepository(Fenetre::class)->find($request->get('id'));
+            if($fenetre != null){
+                $fenetre->setUrl($request->get('url'));
+                $fenetre->setWidth($request->get('width'));
+                $fenetre->setHeight($request->get('height'));
+                $fenetre->setPosx($request->get('posx'));
+                $fenetre->setPosy($request->get('posy'));
+
+                $em->flush();
+            } else {
+                return $this->json(['reponse' => 'Aucune fenêtre existante pour cet Id']);        
+            }
+
+        } else {
+            return $this->json(['reponse' => 'Erreur lors de la modification de la fenêtre']);
+        }
+
+        return $this->json(['reponse' => 'Fenêtre modifiée avec succès']);
+
     }
 
      /**
